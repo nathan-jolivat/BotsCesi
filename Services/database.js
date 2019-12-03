cd09bd7700884ac337c2974799f801b36fd9eb00
Nathan JOLIVAT <nathan@enjoyup.io>
2019-12-03 14:26:19 +0100

    Add DB credentials

--- Services/database.js
+++ Services/database.js
@@ -1,18 +1,20 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-    host: "localhost",
-    user: "yourusername",
-    password: "yourpassword",
-    database: "mydb"
+    host: "10.244.128.161",
+    user: "bot",
+    password: "botcesi",
+    database: "bot"
 });
 
 con.connect(function(err) {
     if (err) throw err;
-    console.log("Connected!");
+    console.log("Connected to DB !");
+    /*
     var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
     con.query(sql, function (err, result) {
         if (err) throw err;
         console.log("1 record inserted");
     });
+    */
 });
--- bot.js
+++ bot.js
@@ -1,15 +1,17 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const token = require();
+var mysql = require('./Services/database');
 
 bot.on('ready', function () {
     console.log("Je suis connecté !")
 });
 
 bot.on('message', message => {
-    if (message.content === 'ping') {
-        message.reply('pong !')
+    if (message.content.substring(0, 1) === '+') {
+        if (message.content === '+ping') {
+            message.reply('ça suffit je suis un bot pas votre esclave ! :(');
+        }
     }
-})
+});
 
-bot.login("NjUxMzk1NDM5Njc3MDE0MDE3.XeZUzw.5HcXp38YEPn5T3FUsE8O8eBZrAE");
+bot.login('NjUxMzk1NDM5Njc3MDE0MDE3.XeZe4g.BvJSzt3YkBDi3eG-6kIDYMkwoQw');
