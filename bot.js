b54a838fdbc841f39400c6457346d8bee3ba42b4
Nathan JOLIVAT <nathan@enjoyup.io>
2019-12-03 14:04:09 +0100

    Add Express & mysql npm package

--- bot.js
+++ bot.js
@@ -1,2 +1,15 @@
-var Discord = require('Discord.io');
-var autreFicher = require('Monfichier.js');
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const token = require();
+
+bot.on('ready', function () {
+    console.log("Je suis connecté !")
+});
+
+bot.on('message', message => {
+    if (message.content === 'ping') {
+        message.reply('pong !')
+    }
+})
+
+bot.login("NjUxMzk1NDM5Njc3MDE0MDE3.XeZUzw.5HcXp38YEPn5T3FUsE8O8eBZrAE");
